39cea63450089057557281aab598d6216bf0a49f
Poul <anrivandloup@gmail.com>
2022-12-05 23:25:38 -0500

    septimo video

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -18,22 +18,64 @@ class HomePage extends StatelessWidget {
             ElevatedButton(
               onPressed: () {
                 tasksReference.get().then((QuerySnapshot value) {
-                /* QuerySnapshot collection = value;
+                  /* QuerySnapshot collection = value;
                  List <QueryDocumentSnapshot> docs = collection.docs;
                  print(docs.length);
                  print(docs[0]);
                  QueryDocumentSnapshot doc = docs[0];
                  print(doc.id);*/
-                 QuerySnapshot collection = value;
-                 collection.docs.forEach((QueryDocumentSnapshot element) {
-                  Map<String,dynamic> myMap = element.data() as Map<String,dynamic>;
-                  print(myMap["title"]);
-                 },
-                 );
+                  QuerySnapshot collection = value;
+                  collection.docs.forEach(
+                    (QueryDocumentSnapshot element) {
+                      Map<String, dynamic> myMap =
+                          element.data() as Map<String, dynamic>;
+                      print(myMap["title"]);
+                    },
+                  );
                 });
               },
               child: Text(
                 "obtener la data",
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                tasksReference.add(
+                  {
+                    "title": "Ir de compras al super 3",
+                    "description": "Debemos de comprar comida para todo el mes",
+                  },
+                ).then((DocumentReference value) {
+                  print(value.id);
+                }).catchError((error) {
+                  print("Ocurrio un error en el Registro");
+                }).whenComplete(() {
+                  print("El resgistor a terminado");
+                });
+              },
+              child: Text(
+                "Agregar documento",
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                tasksReference.doc("oCVGzXDYWhfkZy46leSe").update(
+                  {
+                    "title": "Ir de paseo",
+                    "description": "tenemos que salir muy temprano",
+                  },
+                ).catchError(
+                  (error) {
+                    print(error);
+                  },
+                ).whenComplete(
+                  () {
+                    print("actulaizacion terminada");
+                  },
+                );
+              },
+              child: Text(
+                "Actualizar Documento",
               ),
             ),
           ],
