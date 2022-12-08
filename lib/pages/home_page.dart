24378feaec3ffa479405a5bc049ae3d6b50161e3
Poul <anrivandloup@gmail.com>
2022-12-08 14:51:58 -0500

    decimo segundo video

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,6 +12,32 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBranSecondaryColor,
+      floatingActionButton: InkWell(
+        onTap: () {},
+        borderRadius: BorderRadius.circular(14.0),
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+          decoration: BoxDecoration(
+            color: kBranPrimaryColor,
+            borderRadius: BorderRadius.circular(14.0),
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+              Text(
+                "Nueva Traea",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -54,9 +80,86 @@ class HomePage extends StatelessWidget {
                       ),
                     ),
                     divider10(),
-                  TextFieldSearchWidget(),
+                    TextFieldSearchWidget(),
                   ],
                 ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(14),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Todas mis tareas",
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w600,
+                      color: kBranPrimaryColor.withOpacity(0.85),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 14.0, vertical: 16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(14.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.05),
+                          offset: const Offset(4, 4),
+                          blurRadius: 12.0,
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 8.0, vertical: 2.5),
+                          decoration: BoxDecoration(
+                            color: Color(0xff3e80ff),
+                            borderRadius: BorderRadius.circular(4.0),
+                          ),
+                          child: Text(
+                            "Personal",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 13.0,
+                            ),
+                          ),
+                        ),
+                        divider3(),
+                        Text(
+                          "lorem ipsum dolor sit amet",
+                          style: TextStyle(
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.w600,
+                            color: kBranPrimaryColor.withOpacity(0.85),
+                          ),
+                        ),
+                        Text(
+                          "lorem ipsum dolor sit amet, orem ipsum dolor sit amet",
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w500,
+                            color: kBranPrimaryColor.withOpacity(0.75),
+                          ),
+                        ),
+                        divider6(),
+                        Text(
+                          "10/12/2022",
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w500,
+                            color: kBranPrimaryColor.withOpacity(0.75),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
